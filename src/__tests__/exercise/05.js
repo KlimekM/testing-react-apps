b6357e71ef9941d84e660993536af488ff5d76f5
Michal Klimek <mklimek15@gmail.com>
2022-05-09 10:33:53 -0500

    05 - EC2: complete "Unhappy Path"

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -36,3 +36,29 @@ test(`logging in displays the user's username`, async () => {
   // we render the username.
   expect(screen.getByText(username)).toBeInTheDocument()
 })
+
+test(`logging in without a username`, async () => {
+  render(<Login />)
+  const {password} = buildLoginForm()
+
+  // not going to fill in the username
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent('username required')
+})
+
+test(`logging in without a password`, async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  // not going to fill in the password
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent('password required')
+})
