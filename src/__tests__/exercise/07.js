0ea902b619c026b7657f4f559eb85425bb24a38e
Michal Klimek <mklimek15@gmail.com>
2022-05-10 15:24:55 -0500

    07 - EC1/EC2: complete "Dark Theme" and "Render Method"

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,16 +6,29 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-function Wrapper({children}) {
-  return <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+function renderWithProviders(ui, {theme = 'light', ...options} = {}) {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  )
+
+  return render(ui, {wrapper: Wrapper, ...options})
 }
 
 test('renders with the light styles for the light theme', () => {
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  renderWithProviders(<EasyButton>Easy</EasyButton>)
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  renderWithProviders(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
 
